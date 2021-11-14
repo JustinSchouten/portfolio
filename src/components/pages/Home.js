04b58fa21151fc3aaa43b48afba237c8ef9e262c
Justinschouten97 <302807392@student.rocmondriaan.nl>
2021-11-14 23:25:42 +0100

    Testing website 1 React

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Base/Cards';
+import HeroSection from '../Base/HeroSection';
+import Footer from '../Base/Footer';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
